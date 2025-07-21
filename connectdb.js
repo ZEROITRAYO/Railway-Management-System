c846e67780c18d3049f933e5eca2033301862c56
ZEROITRAYO <117174414+ZEROITRAYO@users.noreply.github.com>
2025-07-21 09:03:57 +0530

    Update connectdb.js

--- connectdb.js
+++ connectdb.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 var connectdb = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "dbms"
+    host: "your_host",
+    user: "your_username",
+    password: "your_password",
+    database: "your_database"
   });
 
 connectdb.connect(function(err) {
